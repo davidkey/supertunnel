9f8f27b748effa3681a6da245196a0561e00b9a2
javawizard@trivergia.com <javawizard@trivergia.com@9038e2a8-f00e-11dd-89c5-9df526a60542>
2009-10-31 17:59:12 +0000

    Server is done; now working on the client.

--- trunk/projects/supertunnel/src/jw/supertunnel/client/Connection.java
+++ trunk/projects/supertunnel/src/jw/supertunnel/client/Connection.java
@@ -1,6 +1,21 @@
 package jw.supertunnel.client;
 
+import java.net.Socket;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
 public class Connection
 {
-    
+
+    public Connection(Socket socket)
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void start()
+    {
+        // TODO Auto-generated method stub
+        
+    }
 }
